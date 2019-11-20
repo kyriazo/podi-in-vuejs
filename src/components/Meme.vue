c0a3fb3ff006d00936a21168949d05386a664c2a
Giannis <gianniskyriazo@gmail.com>
2019-11-20 16:08:22 +0200

    Modifies meme image to use the prop link and switches to mailchimp module form.

--- src/components/Meme.vue
+++ src/components/Meme.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='meme'>
         <div class='meme-image'>
-            <expandable-image :src="require('./../assets/images/meme.png')" v-bind:close-on-background-click='true' ></expandable-image>
+            <expandable-image :src="require('../assets/images/memes/' + image + '.png')" v-bind:close-on-background-click='true' ></expandable-image>
         </div>
         <div class='meme-title'>{{title}}</div>
         <div class='share'><span class='share-icon'></span><button @click="show">SHARE NOW!</button></div>
@@ -17,71 +17,61 @@
                         <div class='main-text'>Είναι στο χέρι σου... ε, στο πόδι σου ήθελα να πω!</div>
                         <div class='sub-text'>Δώσε μου το email σου για να μπορείς να κάνεις share το σύνθημά σου.</div>
                     </div>
-                        <!-- Begin Mailchimp Signup Form -->
-                            <div id="mc_embed_signup">
-                            <form action="https://e-sepia.us5.list-manage.com/subscribe/post?u=4137c3bb43188dbda96b67132&amp;id=1b660cfa21" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
-                                <div id="mc_embed_signup_scroll">
-                                
-                            <div class="mc-field-group">
-                                <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-                            </label>
-                                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-                            </div>
-                                <div id="mce-responses" class="clear">
-                                    <div class="response" id="mce-error-response" style="display:none"></div>
-                                    <div class="response" id="mce-success-response" style="display:none"></div>
-                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4137c3bb43188dbda96b67132_1b660cfa21" tabindex="-1" value=""></div>
-                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                                </div>
+                        <mailchimp-subscribe
+                            url="https://e-sepia.us5.list-manage.com/subscribe/post"
+                            user-id="4137c3bb43188dbda96b67132"
+                            list-id="1b660cfa21"
+                            @error="onError"
+                            @success="onSuccess"
+                        >
+                            <template v-slot="{ subscribe, setEmail, error, success, loading }">
+                            <form @submit.prevent="subscribe">
+                                <input type="email" @input="setEmail($event.target.value)" />
+                                <button type="submit">Submit</button>
+                                <div v-if="error">{{ error }}</div>
+                                <div v-if="success">Yay!</div>
+                                <div v-if="loading">Loading…</div>
                             </form>
-                            </div>
-
-                        <!--End mc_embed_signup-->
-                    <input type='text' placeholder='EMAIL'>
-                    <span class='terms'>Αποδέχομαι τους όρους</span>
-                    <button class='share-button'>ΕΙΣΑΙ ΕΤΟΙΜΟΣ ΝΑ ΚΑΝΕΙΣ SHARE</button>
+                            </template>
+                        </mailchimp-subscribe>
                 </div>
             </div>
         </modal>
-        <!-- <Modal v-model="modalOpen"></Modal> -->
     </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import VueExpandableImage from "vue-expandable-image";
+import MailchimpSubscribe from 'vue-mailchimp-subscribe'
 Vue.use(VueExpandableImage);
-// import Modal from './Modal';
 import vmodal from 'vue-js-modal'
 Vue.use(vmodal)
 
 export default {
     name: 'Meme',
+    components: {
+        MailchimpSubscribe,
+    },
     props: [
         'title',
+        'image',
     ],
     methods: {
-        // openModal () {
-        //     this.modalOpen = !this.modalOpen;
-        // },
         show() {
             this.$modal.show('share-modal');
         },
 
         hide () {
             this.$modal.hide('share-modal');
-        }
+        },
+        onError() {
+            alert('There is an error');
+        },
+        onSuccess() {
+            alert('Succesful');
+        },
     },
-    // components: {
-    //     Modal
-    // },
-    // data() {
-    //     return {
-    //         modalOpen: false
-    //     }
-    // }
-
 }
 </script>
 
