5c1b0a91ec9caf2f92b20713367404542174ce5f
Giannis <gianniskyriazo@gmail.com>
2019-11-20 16:07:11 +0200

    Adds image filenames as props of meme components.

--- src/components/Memes.vue
+++ src/components/Memes.vue
@@ -1,13 +1,13 @@
 <template>
     <div class='memes-container'>
-        <Meme title='LEGalize it!'></Meme>
-        <Meme title='Ελευθερία στα άκρα!'></Meme>
-        <Meme title='ΠΟΔΕΜΟΣ!'></Meme>
-        <Meme title='Καμία ανοχή. Τα γάντια φυλακή'></Meme>
-        <Meme title='Πάνω τα πόδια. Κάτω τα χέρια.'></Meme>
-        <Meme title='Πόδι μπορείς, μπορείς να προκριθείς!'></Meme>
-        <Meme title='I Pasaran!'></Meme>
-        <Meme title='Η αγάπη δέν έχει άκρο.'></Meme>
+        <Meme title='LEGalize it!' image='1'></Meme>
+        <Meme title='Ελευθερία στα άκρα!' image='2'></Meme>
+        <Meme title='ΠΟΔΕΜΟΣ!' image='3'></Meme>
+        <Meme title='Καμία ανοχή. Τα γάντια φυλακή' image='4'></Meme>
+        <Meme title='Πάνω τα πόδια. Κάτω τα χέρια.' image='5'></Meme>
+        <Meme title='Πόδι μπορείς, μπορείς να προκριθείς!' image='6'></Meme>
+        <Meme title='I Pasaran!' image='7'></Meme>
+        <Meme title='Η αγάπη δέν έχει άκρο.' image='8'></Meme>
     </div>
 </template>
 
