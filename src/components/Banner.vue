f2ce44154f4c382c5628795f04597bdf424c86a1
Giannis <gianniskyriazo@gmail.com>
2019-11-22 10:03:28 +0200

    Adds smooth scrolling to the banner component.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,22 +4,22 @@
         <div class='inner-container'>
             <h1>Γίνε κι εσύ <span>IDERistas!</span></h1>
             <div class='main-text'>Οι IDERistas είναι το κίνημα που στόχο έχει να στηρίξει το αναμφισβητητο δικαίωμα της επιλογής, όσο ακραία κι αν είναι αυτή. Να στηρίξει το δικαίωμα μου να γίνω το άκρο που θέλω: <span>Να γίνω πόδι! Και σ' αυτό σε θέλω μαζί μου.</span> Σου ζητώ λοιπόν να βάλεις κι εσύ ένα χέρι για να γίνω πόδι κάνοντας share τα συνθήματα του αγώνα μου.</div>
-            <button @click='scrollMe'>ΞΕΚΙΝΑ ΤΩΡΑ ΤΟΝ ΑΓΩΝΑ!</button>
+            <a class='scroll-button' href='#video' v-smooth-scroll="{ duration: 1000, offset: -100}">ΞΕΚΙΝΑ ΤΩΡΑ ΤΟΝ ΑΓΩΝΑ!</a>
             <img class='hand' src='./../assets/images/hand.png'>
             <div class='video-header'>Μία ιστορία <span>άκρως αληθινή...</span></div>
-            <img class='video' src='./../assets/images/video.png'>
+            <img id='video' class='video' src='./../assets/images/video.png'>
             <img class='logo' src='./../assets/images/ider_logo.png'>
         </div>
     </div>
 </template>
 
 <script>
+import vueSmoothScroll from 'vue2-smooth-scroll'
+import Vue from 'vue'
+Vue.use(vueSmoothScroll)
+
 export default {
     name: 'Banner',
-    methods: {
-        scrollMe() {
-        }
-    }
 }
 </script>
 
