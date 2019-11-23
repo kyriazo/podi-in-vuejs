3058210691f056e324691464a50204f44a38c64c
Kyriazo <gianniskyriazo@gmail.com>
2019-11-23 23:04:36 +0200

    Adds more routes for pre-rendering.

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,26 @@
 module.exports = {
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: [
-        '/',
-        '/1',
-        '/2',
-        '/3',
-        '/4',
-        '/5',
-        '/6',
-        '/7',
-        '/8',
-      ],
-      useRenderEvent: false,
-      headless: true,
-      onlyProduction: true,
-      // renderAfterTime: 30000,
-      renderAfterElementExists: '[data-vue-meta]'
+    pluginOptions: {
+        prerenderSpa: {
+            registry: undefined,
+            renderRoutes: [
+                '/',
+                '/1/',
+                '/2/',
+                '/3/',
+                '/4/',
+                '/5/',
+                '/6/',
+                '/7/',
+                '/8/',
+                '/9/',
+                '/10/',
+                '/11/',
+
+            ],
+            useRenderEvent: false,
+            headless: true,
+            onlyProduction: true,
+            renderAfterElementExists: '[data-vue-meta]'
+        }
     }
-  }
 }
