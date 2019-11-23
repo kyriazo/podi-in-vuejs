6d49946d17e17b7b70353770e5c71d839e081573
Kyriazo <gianniskyriazo@gmail.com>
2019-11-23 23:14:28 +0200

    Fixes names of memes.

--- src/components/Memes.vue
+++ src/components/Memes.vue
@@ -3,13 +3,13 @@
         <Meme title='LEGalize it!' image='1'></Meme>
         <Meme title='Ελευθερία στα άκρα!' image='2'></Meme>
         <Meme title='ΠΟΔΕΜΟΣ!' image='3'></Meme>
-        <Meme title='Καμία ανοχή. Τα γάντια φυλακή' image='4'></Meme>
-        <Meme title='Πάνω τα πόδια. Κάτω τα χέρια.' image='5'></Meme>
+        <Meme title='Καμία ανοχή. Τα γάντια φυλακή!' image='4'></Meme>
+        <Meme title='Πάνω τα πόδια. Κάτω τα χέρια!' image='5'></Meme>
         <Meme title='Πόδι μπορείς, μπορείς να προκριθείς!' image='6'></Meme>
-        <Meme title='I Pasaran!' image='7'></Meme>
-        <Meme title='Η αγάπη δέν έχει άκρο.' image='8'></Meme>
+        <Meme title='No Pasaran!' image='7'></Meme>
+        <Meme title='Η αγάπη δέν έχει άκρο!' image='8'></Meme>
         <Meme title='Λευτεριά στα πόδια μας ΤΩΡΑ!' image='9'></Meme>
-        <Meme title='Liberte fraternite LEGalite' image='10'></Meme>
+        <Meme title='Liberte fraternite LEGalite!' image='10'></Meme>
         <Meme title='Αλληλεγγύη στα κάτω άκρα!' image='11'></Meme>
         <div class='logo-bottom'><span>Powered by:</span><img class='logo'  src='./../assets/images/ider_logo.png'></div>
     </div>
