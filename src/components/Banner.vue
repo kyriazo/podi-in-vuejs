609e87e9349bf5a5b2dd6b5d2216900014904461
Kyriazo <gianniskyriazo@gmail.com>
2019-11-25 01:59:21 +0200

    Removes unwanted code and fixes typo.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,7 +4,7 @@
         <div class='inner-container'>
             <img class='hand' src='./../assets/images/hand.png'>
             <h1>Γίνε κι εσύ <span>IDERistas!</span></h1>
-            <div class='main-text'>Οι IDERistas είναι το κίνημα που στόχο έχει να στηρίξει το αναμφισβητητο δικαίωμα της επιλογής, όσο ακραία κι αν είναι αυτή. Να στηρίξει το δικαίωμα μου να γίνω το άκρο που θέλω: <span>Να γίνω πόδι! Και σ' αυτό σε θέλω μαζί μου.</span> Σου ζητώ λοιπόν να βάλεις κι εσύ ένα χέρι για να γίνω πόδι κάνοντας share τα συνθήματα του αγώνα μου.</div>
+            <div class='main-text'>Οι IDERistas είναι το κίνημα που στόχο έχει να στηρίξει το αναμφισβήτητο δικαίωμα της επιλογής, όσο ακραία κι αν είναι αυτή. Να στηρίξει το δικαίωμα μου να γίνω το άκρο που θέλω: <span>Να γίνω πόδι! Και σ' αυτό σε θέλω μαζί μου.</span> Σου ζητώ λοιπόν να βάλεις κι εσύ ένα χέρι για να γίνω πόδι κάνοντας share τα συνθήματα του αγώνα μου.</div>
             <a class='scroll-button' href='#video' v-smooth-scroll="{ duration: 1000, offset: -140}">ΞΕΚΙΝΑ ΤΩΡΑ ΤΟΝ ΑΓΩΝΑ!</a>
             <div class='video-header'>Μία ιστορία <span>άκρως αληθινή...</span></div>
             <iframe id='video' src="https://player.vimeo.com/video/374894109" width="1200" height="300" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
@@ -20,23 +20,6 @@ Vue.use(vueSmoothScroll)
 
 export default {
     name: 'Banner',
-//     data: {
-//         isFixed : false,
-//     },
-//     mounted() {
-        
-//         $(window).scroll(function (event) {
-//         var scroll = $(window).scrollTop();
-//         var element = $('.banner-container .logo').position().top;
-//         if(scroll<element){
-//             // $('banner-container .logo').removeClass('stuck');
-//             this.isFixed = false;
-//         }else if (scroll>element){
-//             // $('banner-container .logo').addClass('stuck');
-//             this.isFixed = true;
-//         }
-// });
-//     }
 }
 </script>
 
