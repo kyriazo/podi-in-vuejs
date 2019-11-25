d830245d818e86cd1e46997e66a879795309d3ee
Giannis <gianniskyriazo@gmail.com>
2019-11-25 13:49:25 +0200

    Adds share route.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Share from '../views/Share.vue'
 
 Vue.use(VueRouter)
 
@@ -8,6 +9,11 @@ const routes = [{
         path: '/',
         name: 'IDER - Thelonaginopodi',
         component: Home
+    },
+    {
+        path: '/share',
+        name: 'Share',
+        component: Share
     },
     {
         path: '/1',
