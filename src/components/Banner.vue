35a559c3279d4c68cd6160d76024bc756601af27
Giannis <gianniskyriazo@gmail.com>
2019-11-25 13:51:53 +0200

    Adds a wrapper for the video anchor link.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,7 +5,7 @@
             <img class='hand' src='./../assets/images/hand.png'>
             <h1>Γίνε κι εσύ <span>IDERistas!</span></h1>
             <div class='main-text'>Οι IDERistas είναι το κίνημα που στόχο έχει να στηρίξει το αναμφισβήτητο δικαίωμα της επιλογής, όσο ακραία κι αν είναι αυτή. Να στηρίξει το δικαίωμα μου να γίνω το άκρο που θέλω: <span>Να γίνω πόδι! Και σ' αυτό σε θέλω μαζί μου.</span> Σου ζητώ λοιπόν να βάλεις κι εσύ ένα χέρι για να γίνω πόδι κάνοντας share τα συνθήματα του αγώνα μου.</div>
-            <a class='scroll-button' href='#video' v-smooth-scroll="{ duration: 1000, offset: -140}">ΞΕΚΙΝΑ ΤΩΡΑ ΤΟΝ ΑΓΩΝΑ!</a>
+            <div class='video-link'><a class='scroll-button' href='#video' v-smooth-scroll="{ duration: 1000, offset: -140}">ΞΕΚΙΝΑ ΤΩΡΑ ΤΟΝ ΑΓΩΝΑ!</a></div>
             <div class='video-header'>Μία ιστορία <span>άκρως αληθινή...</span></div>
             <iframe id='video' src="https://player.vimeo.com/video/374894109" width="1200" height="300" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
             <img class='logo' src='./../assets/images/ider_logo.png'>
