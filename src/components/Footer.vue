07f88ee5b45299b14dc683d3aab2275da052fb55
Giannis <gianniskyriazo@gmail.com>
2019-11-26 17:25:11 +0200

    Makes cookies and privacy pages open in new tab.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
     <div class='footer-container'>
-        <div class='cp'> ©COPYRIGHT THELONAGINOPODI 2019</div> <span>|</span> <a class='terms-link' href='/privacy'>TERMS OF USE</a>
+        <div class='cp'> ©COPYRIGHT THELONAGINOPODI 2019</div> <span>|</span> <a class='terms-link' href='/privacy' target='newtab'>TERMS OF USE</a>
         <cookie-law>
             <div class='cookie-container' slot-scope="props">
                 <div class='text'>Για την καλύτερη εμπειρία σας στην ιστοσελίδα, χρησιμοποιούμε Cookies.</div>
                 <div class='links'>
-                    <a class='link' href="/cookies">Περισσότερες πληροφορίες</a>
+                    <a class='link' href="/cookies" target='newtab'>Περισσότερες πληροφορίες</a>
                     <button class="accept" @click="props.accept">Συμφωνώ</button>
                 </div>
             </div>
