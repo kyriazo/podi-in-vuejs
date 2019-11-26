aeb8008c6fe3b1b17272f7258a6ff0503c63109b
Giannis <gianniskyriazo@gmail.com>
2019-11-26 12:04:58 +0200

    Adds cookie consent component to footer and styles it.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,26 @@
 <template>
-    <div class='footer-container'><div class='cp'> ©COPYRIGHT THELONAGINOPODI 2019</div> <span>|</span> <a class='terms-link' href='/privacy'>TERMS OF USE</a></div>
+    <div class='footer-container'>
+        <div class='cp'> ©COPYRIGHT THELONAGINOPODI 2019</div> <span>|</span> <a class='terms-link' href='/privacy'>TERMS OF USE</a>
+        <cookie-law>
+            <div class='cookie-container' slot-scope="props">
+                <div class='text'>Για την καλύτερη εμπειρία σας στην ιστοσελίδα, χρησιμοποιούμε Cookies.</div>
+                <div class='links'>
+                    <a class='link' href="/cookies">Περισσότερες πληροφορίες</a>
+                    <button class="accept" @click="props.accept">Συμφωνώ</button>
+                </div>
+            </div>
+        </cookie-law>
+    </div>
 </template>
 
 <script>
+import CookieLaw from 'vue-cookie-law'
+
 export default {
     name: 'Footer',
+    components: {
+        CookieLaw
+    }
 }
 </script>
 
