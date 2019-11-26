81b5ace623d926be9be4d27cab34352c7551f70d
Giannis <gianniskyriazo@gmail.com>
2019-11-26 11:05:40 +0200

    Changes meme image extension requirement to jpg.

--- src/components/Meme.vue
+++ src/components/Meme.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='meme'>
         <div class='meme-image'>
-            <expandable-image :src="require('../assets/images/memes/' + image + '.png')" v-bind:close-on-background-click='true' ></expandable-image>
+            <expandable-image :src="require('../assets/images/memes/' + image + '.jpg')" v-bind:close-on-background-click='true' ></expandable-image>
         </div>
         <div class='meme-title'>{{title}}</div>
         <div class='share'><span class='share-icon'></span><router-link class='router-link' :to= "{ name: 'Share', params: { image: this.image }}" >SHARE NOW!</router-link></div>
