3daa420520bed9299dfe98d8b5fd0f77b5a2be58
Giannis <gianniskyriazo@gmail.com>
2019-11-26 11:06:24 +0200

    Corrects footer link for terms of use.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class='footer-container'><div class='cp'> ©COPYRIGHT THELONAGINOPODI 2019</div> <span>|</span> <a class='terms-link' href='/terms'>TERMS OF USE</a></div>
+    <div class='footer-container'><div class='cp'> ©COPYRIGHT THELONAGINOPODI 2019</div> <span>|</span> <a class='terms-link' href='/privacy'>TERMS OF USE</a></div>
 </template>
 
 <script>
