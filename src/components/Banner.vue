c990bb7d7d5ad076ac1db8869bff0e040f23454f
Giannis <gianniskyriazo@gmail.com>
2019-11-28 17:13:46 +0200

    Changes video from vimeo to youtube.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -7,7 +7,8 @@
             <div class='main-text'>Οι IDERistas είναι το κίνημα που στόχο έχει να στηρίξει το αναμφισβήτητο δικαίωμα της επιλογής, όσο ακραία κι αν είναι αυτή. Να στηρίξει το δικαίωμα μου να γίνω το άκρο που θέλω: <span>Να γίνω πόδι! Και σ' αυτό σε θέλω μαζί μου.</span> Σου ζητώ λοιπόν να βάλεις κι εσύ ένα χέρι για να γίνω πόδι κάνοντας share τα συνθήματα του αγώνα μου.</div>
             <div class='video-link'><a class='scroll-button' href='#video' v-smooth-scroll="{ duration: 1000, offset: -140}">ΞΕΚΙΝΑ ΤΩΡΑ ΤΟΝ ΑΓΩΝΑ!</a></div>
             <div class='video-header'>Μία ιστορία <span>άκρως αληθινή...</span></div>
-            <iframe id='video' src="https://player.vimeo.com/video/375607781" width="1200" height="300" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+            <iframe id='video' width="560" height="315" src="https://www.youtube.com/embed/U0ygfbwN_vg?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <!-- <iframe id='video' src="https://player.vimeo.com/video/375607781" width="1200" height="300" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe> -->
             <a href="https://www.ider-eshop.gr/"><img class='logo' src='./../assets/images/ider_logo.png'/></a>
         </div>
     </div>
