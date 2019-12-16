8bfa1fc05ce2c591ded5e8b5516863ce51abf86b
Giannis <gianniskyriazo@gmail.com>
2019-12-16 11:59:38 +0200

    Revert "Revert "Removes share route, which makes getting emails obsolete, and implements meme sharing directly to share now button.""
    This reverts commit f414009ca451063e2ba48ef6348c5db501ef8f3b.

--- src/components/Memes.vue
+++ src/components/Memes.vue
@@ -1,16 +1,16 @@
 <template>
     <div class='memes-container'>
-        <Meme title='LEGalize it!' image='1'></Meme>
-        <Meme title='Ελευθερία στα άκρα!' image='2'></Meme>
-        <Meme title='ΠΟΔΕΜΟΣ!' image='3'></Meme>
-        <Meme title='Καμία ανοχή. Τα γάντια φυλακή!' image='4'></Meme>
-        <Meme title='Πάνω τα πόδια. Κάτω τα χέρια!' image='5'></Meme>
-        <Meme title='Πόδι μπορείς, μπορείς να προκριθείς!' image='6'></Meme>
-        <Meme title='¡No Pasaran!' image='7'></Meme>
-        <Meme title='Η αγάπη δεν έχει άκρο!' image='8'></Meme>
-        <Meme title='Λευτεριά στα πόδια μας ΤΩΡΑ!' image='9'></Meme>
-        <Meme title='liberté – fraternité - LEGalité!' image='10'></Meme>
-        <Meme title='Αλληλεγγύη στα κάτω άκρα!' image='11'></Meme>
+        <Meme title='LEGalize it!' image='1' hashtag='LEGalize_it'></Meme>
+        <Meme title='Ελευθερία στα άκρα!' image='2' hashtag='Eleutheria_sta_akra'></Meme>
+        <Meme title='ΠΟΔΕΜΟΣ!' image='3' hashtag='PODEMOS'></Meme>
+        <Meme title='Καμία ανοχή. Τα γάντια φυλακή!' image='4' hashtag='Kamia_anohi_Ta_gantia_fulaki'></Meme>
+        <Meme title='Πάνω τα πόδια. Κάτω τα χέρια!' image='5' hashtag='Pano_ta_podia_Kato_ta_heria'></Meme>
+        <Meme title='Πόδι μπορείς, μπορείς να προκριθείς!' image='6' hashtag='Podi_mporeis_na_prokritheis'></Meme>
+        <Meme title='¡No Pasaran!' image='7' hashtag='No_Pasaran'></Meme>
+        <Meme title='Η αγάπη δεν έχει άκρο!' image='8' hashtag='I_agapi_den_ehei_akro'></Meme>
+        <Meme title='Λευτεριά στα πόδια μας ΤΩΡΑ!' image='9' hashtag='Leuteria_sta_podia_mas_TORA'></Meme>
+        <Meme title='liberté – fraternité - LEGalité!' image='10' hashtag='liberté_fraternité_LEGalité'></Meme>
+        <Meme title='Αλληλεγγύη στα κάτω άκρα!' image='11' hashtag='Allileggui_sta_kato_akra'></Meme>
     </div>
 </template>
 
